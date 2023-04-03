49034dbfa3aed1bce6814a90c8a8a758a4bd4d80
Sergey Morozov <morozoffserg@gmail.com>
2023-04-03 22:37:25 +0600

    Old version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Img from './images/room1.jpg' 
 
 const rooms = [
   {title: 'Комната на первом этаже', isEmpty: true, id: 1},
@@ -25,17 +25,69 @@ function RoomList() {
   );
 }
 
+function RoomBox() {
+  return (
+      <div className="box-flex-container">
+          <img src={Img} alt="image" style={{ width: '100%' }} />
+          <h1>Номер на первом этаже</h1>
+          <ul style={{width: '20%'}}>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+          <button style={{ alignSelf: 'self-end' }}>Подробнее</button>
+      </div>
+  )
+}
+
+function RoomGrid() {
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridGap: '16px' }}>
+      <RoomBox />
+      <RoomBox />
+      <RoomBox />
+    </div>   
+  )
+}
+
 function DatePicker() {
+  const [startDate, setStartDate] = useState("2022-09-17")
+  const [endDate, setEndDate] = useState("2022-09-17")
+
+  function handleStartDate(e) {
+    setStartDate(e.target.value)
+  }
+
+  function handleEndDate(e) {
+    setEndDate(e.target.value)
+  }
+  
+
   return (
     <div  className='flex-container'>
       <div className='item'>
         <label>c</label>
-        <input  type="date" value="2022-09-17"/>
+        <input  type="date" onChange={handleStartDate} defaultValue="2022-09-17"/>
       </div>
       <div className='item'>
         <label>до</label>
-        <input  type="date" value="2022-09-24"/>
-      </div>      
+        <input  type="date" onChange={handleEndDate} defaultValue="2022-09-17"/>
+      </div>
+      <p>Выбранные даты: c {startDate} по {endDate}</p>
+      <button>Найти свободный номер</button>
+    </div>
+  );
+}
+
+function MyNavbar() {
+  return (
+    <div className='navbar-flex-container'>
+      <div className="logo">Гостевой дом Ассоль</div>
+      <ul>
+        <li><a href="#home">Главная</a></li>
+        <li><a href="#pricing">Контакты</a></li>
+        <li><a href="#pricing">Расположение</a></li>
+      </ul>
     </div>
   );
 }
@@ -45,9 +97,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Добро пожаловать!</h1>
+      <MyNavbar />
       <DatePicker />
-      <button>Найти свободный номер</button>
+      <RoomGrid />
     </div>
   )
 }
